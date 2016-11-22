1e19eedf60fce5e2d249620bc38c2db26a3af3f5
zpauly <zpauly1996@gmail.com>
2016-11-22 14:49:33 +0800

    refact splash activity

--- app/src/main/java/com/zpauly/githubapp/view/SplashActivity.java
+++ app/src/main/java/com/zpauly/githubapp/view/SplashActivity.java
@@ -1,7 +1,10 @@
 package com.zpauly.githubapp.view;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +14,7 @@ import com.zpauly.githubapp.utils.SPUtil;
 import com.zpauly.githubapp.view.home.HomeActivity;
 import com.zpauly.githubapp.view.login.LoginActivity;
 
+import java.lang.ref.WeakReference;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -18,20 +22,44 @@ import java.util.concurrent.TimeUnit;
  */
 
 public class SplashActivity extends AppCompatActivity {
+    public static final long SPLASH_TIME = 1000;
+    public static final int JUMP_TO_NEXT_ACTIVITY = 0;
+
+    private SplashHandler handler = new SplashHandler(this);
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        SystemClock.sleep(TimeUnit.SECONDS.toMillis(2));
-        if (!SPUtil.getBoolean(this, Constants.LOCAL_CONFIGURATION, Constants.FIRST_USED, true)) {
-            Intent intent = new Intent();
-            intent.setClass(this, HomeActivity.class);
-            startActivity(intent);
-            finish();
-        } else {
-            Intent intent = new Intent();
-            intent.setClass(this, LoginActivity.class);
-            startActivity(intent);
-            finish();
+        handler.sendEmptyMessageDelayed(JUMP_TO_NEXT_ACTIVITY, SPLASH_TIME);
+    }
+
+    private class SplashHandler extends Handler {
+        private WeakReference<Context> contextWeakReference;
+
+        public SplashHandler(Context context) {
+            contextWeakReference = new WeakReference<Context>(context);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == JUMP_TO_NEXT_ACTIVITY) {
+                if (contextWeakReference.get() == null) {
+                    return;
+                }
+                Context context = contextWeakReference.get();
+                if (!SPUtil.getBoolean(context, Constants.LOCAL_CONFIGURATION, Constants.FIRST_USED, true)) {
+                    Intent intent = new Intent();
+                    intent.setClass(context, HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Intent intent = new Intent();
+                    intent.setClass(context, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+            super.handleMessage(msg);
         }
     }
 }
